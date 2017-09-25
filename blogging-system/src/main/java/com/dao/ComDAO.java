4c04d30bd20e19f8a7391e94dfc2edbd69b72df5
nikh95 <32195746+nikh95@users.noreply.github.com>
2017-09-25 19:59:02 +0530

    added CommentDAO.
    Successful

---
+++ blogging-system/src/main/java/com/dao/ComDAO.java
@@ -0,0 +1,15 @@
+package BlogComment;
+
+import java.util.List;
+
+import model.Comment;
+
+
+public interface ComDAO {
+public void createComment(Comment comment);
+//public Comment findComment(long pk);
+//public void update(Comment comment, long pk);
+//public List<Comment> getComments();
+//public void removeComment(long pk);
+//public void removeComment(Comment comment);
+}
