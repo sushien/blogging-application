01e41f23162fdf2570b39fa6ab4a742fcf5b522a
Ketaki Gujar <ketaki.gujar14@gmail.com>
2017-09-26 17:14:53 +0530

    "BlogDAO modified"

--- blogging-system/src/main/java/com/dao/BlogDAO.java
+++ blogging-system/src/main/java/com/dao/BlogDAO.java
@@ -1,20 +1,17 @@
 package com.dao;
 
 import java.util.List;
-
 import com.BloggingApplication.model.Blog;
 
-public interface BlogDAO {
-
-	public void createBlog(Blog blog);
-	
+public interface BlogDAO 
+{
+	public void createBlog(Blog blog);	
 	public void deleteBlog(int blogID);
-public void updateBlog(int blogId,Blog blog);
-	
-	public Blog searchBlogByAuthor(String author);
-	public Blog searchBlogByCategory(String category) ;
-	public Blog searchBlogByTitle(String bgTitle);
-	
+	public void updateBlog(int blogId,Blog blog);	
+	public Blog searchBlogByAuthor(String bgAuthor);
+	public Blog searchBlogByCategory(String bgCategory) ;
+	public Blog searchBlogByKeywordFromTitle(String bgKeyword);	
 	public void viewBlog();
-	
+	public int averageRating( int blogId);
+
 }
