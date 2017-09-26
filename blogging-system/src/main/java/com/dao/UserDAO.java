278fce5a244a9df1298fcf874e10b7e8f109f932
sushmita nair <sushmitanair1@gmail.com>
2017-09-26 18:01:10 +0530

    "Modified UserDAO"

--- blogging-system/src/main/java/com/dao/UserDAO.java
+++ blogging-system/src/main/java/com/dao/UserDAO.java
@@ -1,10 +1,8 @@
-package com.BloggingApplication.DAO;
-import com.BloggingApplication.model.*;
+package com.dao;
+import com.model.User;
 
-
-public interface UserDAO {
-	
+public interface UserDAO 
+{	
 	public void createUser(User user);
 	public boolean findUser(String username,String password);
-
 }
