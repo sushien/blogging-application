4f65205627527eaf9d7250ae34cc3209f6c36036
Nikhil Bodke <nikhilpatil467@gmail.com>
2017-09-28 23:17:13 +0530

    "Added Junit Test file for CommentsImple"

---
+++ blogging-system/src/test/java/CommentsImpleTest.java
@@ -0,0 +1,41 @@
+package com.dao.Impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dao.CommentsDAO;
+import com.model.Blog;
+import com.model.Comments;
+import com.model.User;
+
+public class CommentsImpleTest {
+
+	
+	
+
+/*
+	@Test
+	public void testCreatecomment() {
+		CommentsImple c = new CommentsImple();
+		assertEquals(1,c.getComment(9).get(0).getCommentId());		
+	}*/
+
+/*	@Test
+	public void testGetComment() {
+		CommentsImple c = new CommentsImple();
+		assertEquals("hello",c.getComment(9).get(0).getComments());
+	}*/
+/*
+	@Test
+	public void testRemovecomment() {
+		CommentsImple c = new CommentsImple();
+		c.removecomment(3);
+		assertNotEquals("Hi this is Third Comment",c.getComment(7).get(0).getComments());
+	}
+*/
+}
